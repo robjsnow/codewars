fcd34a1be0cdafb174f63505a4b3c330f524d744
Robert Snow <robjsnow@gmail.com>
2023-03-22 22:14:55 -0500

    added Power.js

---
+++ JavaScript/8Kyu/Power.js
@@ -0,0 +1,8 @@
+function numberToPower(number, power){
+    console.info(Math.log2(1024));
+    let result = 1;
+    for (i = 0; i < exponent; i++) {
+    result *= num;
+    }
+    return result;
+  }
